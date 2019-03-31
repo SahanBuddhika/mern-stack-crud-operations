e4e58faec755f0d9856023c833fa68b14face9c2
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-03-31 23:59:36 +0530

    start to use handlebars

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('./models/db');
 
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const employeeController = require('./controllers/employeeController');
 
 var app = express();
+
+app.set('views',path.join(__dirname,'/views/'));
+
 
 app.listen(3000, () => {
     console.log('Express server started at port : 3000');
