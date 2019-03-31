2541e69a01d0185856de01ad5c5a00e1870cf20f
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-03-31 23:45:46 +0530

    start to edit express.js file

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongose");
+const mongoose = require("mongoose");
 
 mongoose.connect('mongodb+srv://buddhika:Sa**1994@cluster0-8gxra.mongodb.net/test?retryWrites=true',{useNewUrlPaser:true},(err)=>{
     if(!err){ console.log('MongoDb  Connected Succefully')}
     else{  console.log('Error in DB Connection :' + err  )}
 });
 
-
+require('./employee.model');
